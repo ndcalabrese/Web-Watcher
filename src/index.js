41f3ff8ad859a5a7b690ec21279b453e49cfe5cd
ndcalabrese <ndcalabrese@gmail.com>
2024-12-13 01:12:52 -0500

    wip summore

--- src/index.js
+++ src/index.js
@@ -23,55 +23,61 @@ const makeCall = async () => {
   return resp;
 }
 
-const dateTime = format(new Date(), 'yyyy-MM-dd-hh-mm');
-const htmlData = (await makeCall()).data;
-const dom = new JSDOM(htmlData);
-const bodyContent = dom.window.document.body.innerHTML;
+const scrapeyScrape = async (interval) => {
+  const dateTime = format(new Date(), 'yyyy-MM-dd-hh-mm');
+  const htmlData = (await makeCall()).data;
+  const dom = new JSDOM(htmlData);
+  const bodyContent = dom.window.document.body.innerHTML;
 
-try {
-  const differ = new HtmlDiffer({
-    ignoreAttributes: [
-      'src'
-    ],
-    compareAttributesAsJSON: [],
-    ignoreWhitespaces: true,
-    ignoreComments: true,
-    ignoreEndTags: false,
-    ignoreDuplicateAttributes: false
-  });
-  const prevContent = fs.readFileSync(`${outputDir}/prev.txt`, 'utf8');
-  const diff = differ.diffHtml(bodyContent, prevContent);
-  const isEqual = differ.isEqual(bodyContent, prevContent);
-  if (isEqual) {
-    console.log(`No changes - ${dateTime}`);
-  } else {
-    console.log(`CHANGES DETECTED - ${dateTime}`);
-    const diffText = getDiffText(
-      diff,
-      { charsAroundDiff: 80 }
-    );
-    fs.writeFileSync(
-      `${outputDir}/diff_${dateTime}.txt`,
-      JSON.stringify(diffText)
-    );
-    logDiffText(
-      diff,
-      { charsAroundDiff: 80 }
-    );
-    // send notification
-  }
-  fs.writeFileSync(
-    `${outputDir}/prev.txt`,
-    bodyContent
-  );
-} catch (err) {
-  console.log(err);
-  if (!fs.existsSync(outputDir)) {
-    console.log('creating directory');
-    fs.mkdirSync(outputDir, { recursive: true });
+  try {
+    const differ = new HtmlDiffer({
+      ignoreAttributes: [
+        'src'
+      ],
+      compareAttributesAsJSON: [],
+      ignoreWhitespaces: true,
+      ignoreComments: true,
+      ignoreEndTags: false,
+      ignoreDuplicateAttributes: false
+    });
+    const prevContent = fs.readFileSync(`${outputDir}/prev.txt`, 'utf8');
+    const diff = differ.diffHtml(bodyContent, prevContent);
+    const isEqual = differ.isEqual(bodyContent, prevContent);
+    if (isEqual) {
+      console.log(`No changes - ${dateTime}`);
+    } else {
+      console.log(`CHANGES DETECTED - ${dateTime}`);
+      const diffText = getDiffText(
+        diff,
+        { charsAroundDiff: 80 }
+      );
+      fs.writeFileSync(
+        `${outputDir}/diff_${dateTime}.txt`,
+        JSON.stringify(diffText)
+      );
+      logDiffText(
+        diff,
+        { charsAroundDiff: 80 }
+      );
+      // send notification
+    }
     fs.writeFileSync(
       `${outputDir}/prev.txt`,
       bodyContent
     );
+  } catch (err) {
+    console.log(err);
+    if (!fs.existsSync(outputDir)) {
+      console.log('creating directory');
+      fs.mkdirSync(outputDir, { recursive: true });
+      fs.writeFileSync(
+        `${outputDir}/prev.txt`,
+        bodyContent
+      );
+    }
   }
-}
+  const ms = interval * 60 * 1000;
+  setTimeout(() => scrapeyScrape(interval), ms);
+};
+
+scrapeyScrape(30);
