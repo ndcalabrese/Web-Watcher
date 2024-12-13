34fd90b964dd4db89a55901da6f74c257967cb9a
ndcalabrese <ndcalabrese@gmail.com>
2024-12-12 19:41:15 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const ZipPlugin = require('zip-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  optimization: {
+    minimize: true
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true
+  },
+  mode: 'production',
+  optimization: {
+    usedExports: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /\.env$/,
+    }),
+    new Dotenv({
+      path: './.env', // Path to your .env file
+      safe: false,     // Optional: Validate against .env.example
+    }),
+    new ZipPlugin({
+      filename: 'web-watcher.zip'
+    })
+  ],
+  target: 'node'
+};
